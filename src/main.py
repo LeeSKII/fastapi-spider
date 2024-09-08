3e719ccdaf5cbff8467b7fbdd517d30d0e10026e
DESKTOP-2CGHMPC\WLZX <leeskiikseel@outlook.com>
2024-09-08 23:47:40 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import re
 from fastapi import FastAPI
 import requests
+from bs4 import BeautifulSoup
 from utils.weather import get_weather_data
 
 app = FastAPI()
@@ -103,6 +104,54 @@ async def province_dict(country_code:str):
             "name": name
         })
     return result
+
+# TODO: 实现天气预报
+@app.get('/weather/forecast/{city_id}')
+async def weather(city_id:str):
+    # 定义中文键名到英文键名的映射
+    key_mapping = {
+        '时间': 'time',
+        '天气': 'weather',
+        '气温': 'temperature',
+        '降水': 'precipitation',
+        '风速': 'wind_speed',
+        '风向': 'wind_direction',
+        '气压': 'pressure',
+        '湿度': 'humidity',
+        '云量': 'cloud_cover'
+    }
+    try:
+        response = requests.get(f'https://weather.cma.cn/web/weather/{city_id}.html')
+        if response.status_code == 200:
+            html_content = BeautifulSoup(response.content, 'lxml')
+            table = html_content.find('table', class_='hour-table')
+            rows = table.find_all('tr')
+
+            # 获取时间列表（列标题）
+            time_slots = [td.text.strip() for td in rows[0].find_all('td')[1:]]
+
+            # 初始化结果列表
+            result = [{} for _ in time_slots]
+
+            for row in rows:  # 跳过表头行
+                cells = row.find_all('td')
+                if cells:
+                    key = cells[0].text.strip().replace('', '').replace(' ', '')
+                    english_key = key_mapping.get(key, key)  # 使用映射获取英文键名
+                    for i, cell in enumerate(cells[1:]):
+                        if '天气' in key:
+                            value = cell.find('img')['src'].split('/')[-1] if cell.find('img') else ''
+                        else:
+                            value = cell.text.strip()
+                        result[i][english_key] = value
+        
+
+            return result
+        else:
+            return {"message": "Error: " + str(response.status_code)}
+    except requests.exceptions.RequestException as e:
+        return {"message": "Error: " + str(e)} 
+
 
 @app.get('/map/weather/{day}')
 async def weather_map(day:str):
